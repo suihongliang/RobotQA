b2ceea5ece29ad5a7dd075a8ef46ebefa0154681
zhangyu <yu.zhang@jian24.com>
2019-03-29 15:50:23 +0800

    add backenduser filter by is seller

--- crm/restapi/views.py
+++ crm/restapi/views.py
@@ -209,6 +209,7 @@ class BackendUserViewSet(CompanyFilterViewSet,
     queryset = BackendUser.objects.filter(
         is_superuser=False, is_active=True, is_staff=False).order_by('created')
     serializer_class = BackendUserSerializer
+    filterset_fields = ('role__is_seller', )
     lookup_url_kwarg = 'mobile'
     lookup_field = 'mobile'
 
