2e795ecfdd9eaead0fe4669d41a0b9c795cc690b
zhangyu <yu.zhang@jian24.com>
2019-03-29 14:34:44 +0800

    user mobile update and create

--- crm/restapi/serializers.py
+++ crm/restapi/serializers.py
@@ -111,6 +111,46 @@ class BackendUserSerializer(serializers.ModelSerializer):
                 company_id=company_id)
         return fields
 
+    def create(self, validated_data):
+        ModelClass = self.Meta.model
+        info = model_meta.get_field_info(ModelClass)
+        many_to_many = {}
+        for field_name, relation_info in info.relations.items():
+            if relation_info.to_many and (field_name in validated_data):
+                many_to_many[field_name] = validated_data.pop(field_name)
+
+        try:
+            instance = ModelClass._default_manager.create(**validated_data)
+        except TypeError:
+            # tb = traceback.format_exc()
+            raise TypeError('create backenduser type error')
+
+        # Save many-to-many relationships after the instance is created.
+        if many_to_many:
+            for field_name, value in many_to_many.items():
+                field = getattr(instance, field_name)
+                field.set(value)
+
+        instance.set_password(instance.password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        # raise_errors_on_nested_writes('update', self, validated_data)
+        info = model_meta.get_field_info(instance)
+
+        for attr, value in validated_data.items():
+            if attr in info.relations and info.relations[attr].to_many:
+                field = getattr(instance, attr)
+                field.set(value)
+            else:
+                setattr(instance, attr, value)
+        if 'password' in validated_data.keys():
+            instance.set_password(validated_data['password'])
+        instance.save()
+
+        return instance
+
     class Meta:
         model = BackendUser
         fields = (
