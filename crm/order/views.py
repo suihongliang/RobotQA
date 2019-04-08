a46822f9d356cf9991cb5baefd9bbab89dc7e7a6
linbin <lin1253007885>
2019-04-08 15:29:49 +0800

    雨花订单导出

--- crm/order/views.py
+++ crm/order/views.py
@@ -2,6 +2,8 @@ import urllib
 
 import requests
 from django.conf import settings
+from django.http import HttpResponse
+from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ViewSet
@@ -23,13 +25,35 @@ class OrderViewSet(ViewSet):
     def list(self, request):
         query_string = request.META['QUERY_STRING']
         params = dict(urllib.parse.parse_qsl(query_string))
-        params['store_code_havings'] = 'C0001'
         result = requests.get(settings.ERP_JIAN24_URL + '/merchant/order', params=params)
         return Response(result.json())
 
     def retrieve(self, request, pk):
         query_string = request.META['QUERY_STRING']
         params = dict(urllib.parse.parse_qsl(query_string))
-        params = {'store_code_havings': 'C0001'}
         result = requests.get(settings.ERP_JIAN24_URL + '/merchant/order-detail/{0}'.format(pk), params=params)
         return Response(result.json())
+
+
+class OrderViewSetExport(ViewSet):
+    permission_classes = (AllowAny,)
+
+    @list_route()
+    def export(self, request):
+        query_string = request.META['QUERY_STRING']
+        params = dict(urllib.parse.parse_qsl(query_string))
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        result = requests.get(settings.ERP_JIAN24_URL + '/merchant/order/crm_export/', params=params)
+        response.write(result.content)
+        response['Content-Disposition'] = 'attachment; filename="{0}.xls"'.format(urllib.parse.quote_plus('订单列表'))
+        return response
+
+    @list_route()
+    def export_detail(self, request):
+        query_string = request.META['QUERY_STRING']
+        params = dict(urllib.parse.parse_qsl(query_string))
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        result = requests.get(settings.ERP_JIAN24_URL + '/merchant/order/export_detail/', params=params)
+        response.write(result.content)
+        response['Content-Disposition'] = 'attachment; filename="{0}.xls"'.format(urllib.parse.quote_plus('订单明细列表'))
+        return response
