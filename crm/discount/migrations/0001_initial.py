d6a94c2433dfca7e6c3126778952a453700fe3cb
linbin <lin1253007885>
2019-04-18 12:00:23 +0800

    [fix]clean and reset migrations file

--- crm/discount/migrations/0001_initial.py
+++ crm/discount/migrations/0001_initial.py
@@ -89,4 +89,42 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
                                     to=settings.AUTH_USER_MODEL, verbose_name='后台用户'),
         ),
+        migrations.AddField(
+            model_name='sendcoupon',
+            name='coupon',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='discount.Coupon',
+                                    verbose_name='优惠券'),
+        ),
+        migrations.AddField(
+            model_name='sendcoupon',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.UserInfo', verbose_name='客户'),
+        ),
+        migrations.AddField(
+            model_name='pointrecord',
+            name='rule',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                    to='discount.CoinRule', verbose_name='积分规则'),
+        ),
+        migrations.AddField(
+            model_name='pointrecord',
+            name='seller',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                    to=settings.AUTH_USER_MODEL, verbose_name='后台管理'),
+        ),
+        migrations.AddField(
+            model_name='pointrecord',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.UserInfo', verbose_name='用户'),
+        ),
+        migrations.AddField(
+            model_name='coinrule',
+            name='qrcode',
+            field=models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                       related_name='coin_rule', to='discount.CoinQRCode', verbose_name='二维码'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='coinrule',
+            unique_together={('category', 'company_id')},
+        ),
     ]
