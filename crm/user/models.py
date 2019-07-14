1a7d40685e9148a6d25c4bd90354b82803332037
linbin <lin1253007885>
2019-07-15 01:00:51 +0800

    修正购买意向模型子段名称

--- crm/user/models.py
+++ crm/user/models.py
@@ -612,8 +612,8 @@ class WebsiteConfig(models.Model):
 
 class SubTitle(models.Model):
     no = models.IntegerField(verbose_name="序号", default=0)
-    name = models.CharField(verbose_name="标题名", max_length=200)
-    is_single = models.BooleanField(verbose_name="是否单选")
+    name = models.CharField(verbose_name="标题名", max_length=200, blank=True, null=True)
+    is_single = models.BooleanField(verbose_name="是否单选", default=True)
     company_id = models.CharField(
         verbose_name="公司id", max_length=20,
         null=True)
@@ -626,8 +626,8 @@ class SubTitle(models.Model):
 
 
 class SubTitleChoice(models.Model):
-    sub_title = models.ForeignKey(SubTitle, verbose_name="意向购买问题", on_delete=models.CASCADE)
-    content = models.CharField(verbose_name="选项内容", max_length=200)
+    sub_title = models.ForeignKey(SubTitle, verbose_name="意向购买问题", on_delete=models.CASCADE, blank=True, null=True)
+    content = models.CharField(verbose_name="选项内容", max_length=200,  blank=True, null=True)
 
     class Meta:
         verbose_name = verbose_name_plural = "意向购买选项"
@@ -638,8 +638,8 @@ class SubTitleChoice(models.Model):
 class SubTitleRecord(models.Model):
     user = models.ForeignKey(
         BaseUser, on_delete=models.CASCADE, verbose_name="用户")
-    choice = models.ForeignKey(SubTitleChoice, verbose_name="意向购买问题", on_delete=models.CASCADE)
-    is_choose = models.BooleanField(verbose_name="是否单选")
+    choice = models.ForeignKey(SubTitleChoice, verbose_name="意向购买问题", on_delete=models.CASCADE, blank=True, null=True)
+    is_choose = models.BooleanField(verbose_name="是否", default=True)
 
     class Meta:
         verbose_name = verbose_name_plural = "意向记录"
