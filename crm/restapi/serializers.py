360156faa0b54937a968e3b39eabe328a2662090
linbin <lin1253007885>
2019-07-30 10:27:38 +0800

    销售信息修改

--- crm/restapi/serializers.py
+++ crm/restapi/serializers.py
@@ -288,6 +288,7 @@ class BackendUserSerializer(serializers.ModelSerializer):
             'name',
             'group',
             'group_id',
+            'avatar',
         )
         read_only_fields = ('created',)
         validators = [
@@ -711,6 +712,24 @@ class SellerSerializer(AssignUserCompanySerializer):
         if hasattr(qrcode_info, 'company_id') and not qrcode_info.company_id == instance.user.company_id:
             return None
         return QRCodeSerializer(qrcode_info).data if qrcode_info else None
+
+    def get_profession(self, instance):
+        back_user = BackendUser.objects.filter(mobile=instance.user.mobile, company_id=instance.user.company_id).first()
+        if not back_user:
+            return None
+        return back_user.role.name
+
+    def get_name(self, instance):
+        back_user = BackendUser.objects.filter(mobile=instance.user.mobile, company_id=instance.user.company_id).first()
+        if not back_user:
+            return None
+        return back_user.name
+
+    def get_avatar(self, instance):
+        back_user = BackendUser.objects.filter(mobile=instance.user.mobile, company_id=instance.user.company_id).first()
+        if not back_user:
+            return None
+        return back_user.avatar
 
     class Meta:
         model = Seller
