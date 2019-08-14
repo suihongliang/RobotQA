38c7a978253a6eee5e95431b788bdc09b97f98bc
linbin <lin1253007885>
2019-08-14 22:23:57 +0800

    水吧获取订单

--- crm/restapi/views.py
+++ crm/restapi/views.py
@@ -1415,11 +1415,13 @@ def bar_order_record(request):
     mobile = request.GET.get('mobile')
     limit = request.GET.get('limit', 10)  # 每页最大数量
     page = request.GET.get('page', 1)  # 页码
+    desc = request.GET.get('desc', 1)  # 页码
     params = {
         "company_id": company_id,
         "mobile": mobile,
         "limit": limit,
-        "page": page
+        "page": page,
+        "desc": desc
     }
     res = requests.get(settings.ERP_JIAN24_URL + "/crm/bar-order-record", params=params)
     return cores(res.json())
